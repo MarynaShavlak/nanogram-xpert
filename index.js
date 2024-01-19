f5db1620eb3ff0144dee916fb213dd43d907afd4
Maryna Shavlak <shavlakmaryna@gmail.com>
2024-01-19 20:08:58 +0200

    add area to click settings

--- index.js
+++ index.js
@@ -3,9 +3,154 @@ const ctx = canvas.getContext('2d');
 const cell = 17;
 const width = 255;
 const height = 255;
+const heartConfig = [
+  {
+    color: '#212121',
+    cells: [
+      [3, 2],
+      [4, 2],
+      [10, 2],
+      [11, 2],
+      [3, 2],
+      [2, 3],
+      [5, 3],
+      [9, 3],
+      [12, 3],
+      [1, 4],
+      [6, 4],
+      [8, 4],
+      [13, 4],
+      [1, 5],
+      [7, 5],
+      [13, 5],
+      [1, 6],
+      [13, 6],
+      [2, 7],
+      [3, 8],
+      [4, 9],
+      [5, 10],
+      [6, 11],
+      [7, 12],
+      [12, 7],
+      [11, 8],
+      [10, 9],
+      [9, 10],
+      [8, 11],
+    ],
+  },
+  {
+    color: 'red',
+    cells: [
+      [2, 6],
+      [3, 7],
+      [4, 8],
+      [5, 9],
+      [6, 10],
+    ],
+  },
+  {
+    color: 'orange',
+    cells: [
+      [2, 5],
+      [3, 6],
+      [4, 7],
+      [5, 8],
+      [6, 9],
+    ],
+  },
+  {
+    color: 'yellow',
+    cells: [
+      [2, 4],
+      [3, 5],
+      [4, 6],
+      [5, 7],
+      [6, 8],
+    ],
+  },
+  {
+    color: 'chartreuse',
+    cells: [
+      [3, 4],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+    ],
+  },
+  {
+    color: 'aqua',
+    cells: [
+      [3, 3],
+      [4, 4],
+      [5, 5],
+      [6, 6],
+    ],
+  },
+  {
+    color: 'blue',
+    cells: [
+      [4, 3],
+      [5, 4],
+      [6, 5],
+    ],
+  },
+];
+
+const notColoredCells = [
+  [7, 6],
+  [7, 7],
+  [7, 8],
+  [7, 9],
+  [7, 10],
+  [7, 11],
+  [8, 5],
+  [8, 6],
+  [8, 7],
+  [8, 8],
+  [8, 9],
+  [8, 10],
+  [9, 4],
+  [9, 5],
+  [9, 6],
+  [9, 7],
+  [9, 8],
+  [9, 9],
+  [10, 3],
+  [10, 4],
+  [10, 5],
+  [10, 6],
+  [10, 7],
+  [10, 8],
+  [11, 3],
+  [11, 4],
+  [11, 5],
+  [11, 6],
+  [11, 7],
+  [12, 4],
+  [12, 5],
+  [12, 6],
+];
 
 grid();
 drawHeart();
+
+let color;
+const colorInput = document.querySelector('.color-input');
+colorInput.addEventListener('input', function () {
+  color = this.value;
+});
+
+canvas.onclick = function (e) {
+  let x = e.offsetX;
+  let y = e.offsetY;
+  let cellIndexByX = Math.floor(x / cell);
+  let cellIndexByY = Math.floor(y / cell);
+  if (isInsideHeart(cellIndexByX, cellIndexByY)) {
+    ctx.fillStyle = color;
+    ctx.fillRect(cellIndexByX * cell, cellIndexByY * cell, 17, 17);
+    ctx.fill();
+  }
+};
 
 function grid() {
   ctx.strokeStyle = 'black';
@@ -29,159 +174,24 @@ function drawCell(x, y, color) {
 }
 
 function drawHeart() {
-  const heartConfig = [
-    {
-      color: '#212121',
-      cells: [
-        [3, 2],
-        [4, 2],
-        [10, 2],
-        [11, 2],
-        [3, 2],
-        [2, 3],
-        [5, 3],
-        [9, 3],
-        [12, 3],
-        [1, 4],
-        [6, 4],
-        [8, 4],
-        [13, 4],
-        [1, 5],
-        [7, 5],
-        [13, 5],
-        [1, 6],
-        [13, 6],
-        [2, 7],
-        [3, 8],
-        [4, 9],
-        [5, 10],
-        [6, 11],
-        [7, 12],
-        [12, 7],
-        [11, 8],
-        [10, 9],
-        [9, 10],
-        [8, 11],
-      ],
-    },
-    {
-      color: 'red',
-      cells: [
-        [2, 6],
-        [3, 7],
-        [4, 8],
-        [5, 9],
-        [6, 10],
-      ],
-    },
-    {
-      color: 'orange',
-      cells: [
-        [2, 5],
-        [3, 6],
-        [4, 7],
-        [5, 8],
-        [6, 9],
-      ],
-    },
-    {
-      color: 'yellow',
-      cells: [
-        [2, 4],
-        [3, 5],
-        [4, 6],
-        [5, 7],
-        [6, 8],
-      ],
-    },
-    {
-      color: 'chartreuse',
-      cells: [
-        [3, 4],
-        [4, 5],
-        [5, 6],
-        [6, 7],
-      ],
-    },
-    {
-      color: 'aqua',
-      cells: [
-        [3, 3],
-        [4, 4],
-        [5, 5],
-        [6, 6],
-      ],
-    },
-    {
-      color: 'blue',
-      cells: [
-        [4, 3],
-        [5, 4],
-        [6, 5],
-      ],
-    },
-  ];
-
   heartConfig.forEach(({ color, cells }) =>
     cells.forEach(([x, y]) => drawCell(x, y, color)),
   );
   setDigitCells();
 }
-//
 
 function setDigitCells() {
   ctx.fillStyle = '#212121';
   ctx.font = '14px Arial';
-  ctx.fillText('1', cell * 7 + cell / 4, cell * 6 + cell / 1.21);
-  ctx.fillText('2', cell * 7 + cell / 4, cell * 7 + cell / 1.21);
-  ctx.fillText('3', cell * 7 + cell / 4, cell * 8 + cell / 1.21);
-  ctx.fillText('4', cell * 7 + cell / 4, cell * 9 + cell / 1.21);
-  ctx.fillText('5', cell * 7 + cell / 4, cell * 10 + cell / 1.21);
-  ctx.fillText('6', cell * 7 + cell / 4, cell * 11 + cell / 1.21);
 
-  ctx.fillText('1', cell * 8 + cell / 4, cell * 5 + cell / 1.21);
-  ctx.fillText('2', cell * 8 + cell / 4, cell * 6 + cell / 1.21);
-  ctx.fillText('3', cell * 8 + cell / 4, cell * 7 + cell / 1.21);
-  ctx.fillText('4', cell * 8 + cell / 4, cell * 8 + cell / 1.21);
-  ctx.fillText('5', cell * 8 + cell / 4, cell * 9 + cell / 1.21);
-  ctx.fillText('6', cell * 8 + cell / 4, cell * 10 + cell / 1.21);
-
-  ctx.fillText('1', cell * 9 + cell / 4, cell * 4 + cell / 1.21);
-  ctx.fillText('2', cell * 9 + cell / 4, cell * 5 + cell / 1.21);
-  ctx.fillText('3', cell * 9 + cell / 4, cell * 6 + cell / 1.21);
-  ctx.fillText('4', cell * 9 + cell / 4, cell * 7 + cell / 1.21);
-  ctx.fillText('5', cell * 9 + cell / 4, cell * 8 + cell / 1.21);
-  ctx.fillText('6', cell * 9 + cell / 4, cell * 9 + cell / 1.21);
-
-  ctx.fillText('1', cell * 10 + cell / 4, cell * 3 + cell / 1.21);
-  ctx.fillText('2', cell * 10 + cell / 4, cell * 4 + cell / 1.21);
-  ctx.fillText('3', cell * 10 + cell / 4, cell * 5 + cell / 1.21);
-  ctx.fillText('4', cell * 10 + cell / 4, cell * 6 + cell / 1.21);
-  ctx.fillText('5', cell * 10 + cell / 4, cell * 7 + cell / 1.21);
-  ctx.fillText('6', cell * 10 + cell / 4, cell * 8 + cell / 1.21);
-
-  ctx.fillText('1', cell * 11 + cell / 4, cell * 3 + cell / 1.21);
-  ctx.fillText('2', cell * 11 + cell / 4, cell * 4 + cell / 1.21);
-  ctx.fillText('3', cell * 11 + cell / 4, cell * 5 + cell / 1.21);
-  ctx.fillText('4', cell * 11 + cell / 4, cell * 6 + cell / 1.21);
-  ctx.fillText('5', cell * 11 + cell / 4, cell * 7 + cell / 1.21);
-
-  ctx.fillText('1', cell * 12 + cell / 4, cell * 4 + cell / 1.21);
-  ctx.fillText('2', cell * 12 + cell / 4, cell * 5 + cell / 1.21);
-  ctx.fillText('3', cell * 12 + cell / 4, cell * 6 + cell / 1.21);
+  notColoredCells.forEach(([x, y], index) => {
+    const offsetX = cell * x + cell / 4;
+    const offsetY = cell * y + cell / 1.21;
+    ctx.fillText((index % 6) + 1, offsetX, offsetY);
+  });
 }
 
-canvas.onclick = function (e) {
-  let x = e.offsetX;
-  let y = e.offsetY;
-  let cellIndexByX = Math.floor(x / cell);
-  let cellIndexByY = Math.floor(y / cell);
-
-  ctx.fillRect(cellIndexByX * cell, cellIndexByY * cell, 17, 17);
-  ctx.fillStyle = 'tomato';
-  ctx.fill();
-
-  // canvas.onmouseup = function () {
-  //   canvas.onmousemove = null;
-  // };
-};
+function isInsideHeart(x, y) {
+  const isEmpty = notColoredCells.find(el => el[0] === x && el[1] === y);
+  return !!isEmpty;
+}
