2f8bb9ae3071616f4086cdfb0d2c70a19ac2b9fe
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-28 18:19:46 +0300

    add fixes for code

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 import {canvas, } from "./config/config.js";
 import {drawGrid} from "./draw/drawGrid.js";
 import {drawDigitTask} from "./draw/drawDigitTask.js";
-
 import {handleCanvasClick} from "./eventHandlers/handleCanvasClick.js";
 import {handleCanvasMouse, handleCanvasMouseLeave} from "./eventHandlers/handleCanvasMouse.js";
 
 
-
 export let color;
+export let filledCells = {};
 const colorInput = document.querySelector('.color-input');
 colorInput.addEventListener('input', function () {
   color = this.value;
 });
 
 
-
-export let filledCells = {};
-
 drawDigitTask();
 drawGrid();
-
-
-
 
 canvas.addEventListener('click', handleCanvasClick);
 canvas.addEventListener('mousemove', handleCanvasMouse);
